a9de53830fdd543da80972f5d049a8f20c67969a
Rehankhann007 <rehankhann77088@gmail.com>
2026-06-21 13:50:37 +0530

    otp fix

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -29,7 +29,7 @@ function Register() {
       const data = await res.json();
 
       if (data.success) {
-        
+        alert("OTP Sent Successfully");
         setOtpSent(true);
       } else {
         alert(data.message);
@@ -61,7 +61,7 @@ const resendOtp = async () => {
     const data = await res.json();
 
     if (data.success) {
-      
+      alert("New OTP Sent");
     } else {
       alert(data.message);
     }
@@ -97,7 +97,7 @@ const resendOtp = async () => {
       const data = await res.json();
 
       if (data.success) {
-        
+        alert("Account Created Successfully");
 
         navigate("/login");
       } else {
@@ -111,8 +111,8 @@ const resendOtp = async () => {
 
 
   return (
-  <div className="mt-10 flex justify-center items-center from-orange-50 to-orange-100 px-4 overflow-hidden">
-    <div className="bg-white p-8 rounded-xl shadow-2xl w-full max-w-md">
+  <div className="min-h-screen flex justify-center items-center bg-gray-100">
+    <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
       <h2 className="text-3xl font-bold text-center text-orange-500 mb-6">
         Create Account
       </h2>
@@ -122,7 +122,7 @@ const resendOtp = async () => {
         placeholder="Full Name"
         value={name}
         onChange={(e) => setName(e.target.value)}
-        className="w-full border border-gray-300 p-3 rounded-lg mb-4"
+        className="w-full border p-3 rounded-lg mb-4"
       />
 
       <input
@@ -130,7 +130,7 @@ const resendOtp = async () => {
         placeholder="Email Address"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        className="w-full border border-gray-300 p-3 rounded-lg mb-4"
+        className="w-full border p-3 rounded-lg mb-4"
       />
 
       <input
@@ -138,13 +138,13 @@ const resendOtp = async () => {
         placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
-        className="w-full border border-gray-300 p-3 rounded-lg mb-4"
+        className="w-full border p-3 rounded-lg mb-4"
       />
 
       {!otpSent ? (
         <button
           onClick={sendOtp}
-          className="w-full bg-orange-500 hover:cursor-pointer text-white py-3 rounded-lg font-semibold hover:bg-orange-600"
+          className="w-full bg-orange-500 text-white py-3 rounded-lg font-semibold hover:bg-orange-600"
         >
           Send OTP
         </button>
@@ -155,7 +155,7 @@ const resendOtp = async () => {
             placeholder="Enter OTP"
             value={otp}
             onChange={(e) => setOtp(e.target.value)}
-            className="w-full border border-gray-300 p-3 rounded-lg mb-4"
+            className="w-full border p-3 rounded-lg mb-4"
           />
 
         {otpSent && (
@@ -164,14 +164,14 @@ const resendOtp = async () => {
 
     <button
       onClick={verifyOtp}
-      className="w-full bg-green-500 hover:cursor-pointer text-white py-3 rounded-lg mb-3"
+      className="w-full bg-green-500 text-white py-3 rounded-lg mb-3"
     >
       Verify OTP
     </button>
 
     <button
       onClick={resendOtp}
-      className="w-full bg-blue-500 hover:cursor-pointer text-white py-3 rounded-lg"
+      className="w-full bg-blue-500 text-white py-3 rounded-lg"
     >
       Resend OTP
     </button>
@@ -233,7 +233,9 @@ const resendOtp = async () => {
             JSON.stringify(data.user)
           );
 
-          
+          alert(
+            "Google Account Created Successfully"
+          );
 
           navigate("/");
           window.location.reload();
