6312c4ed01e3ceee3e51397c767fa270a069b1ff
Rehankhann007 <rehankhann77088@gmail.com>
2026-06-21 15:51:23 +0530

    scroll

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -110,7 +110,7 @@ const resendOtp = async () => {
 
 
   return (
-  <div className="min-h-screen flex justify-center items-center bg-gray-100">
+  <div className="mt-10 flex justify-center items-center bg-gray-100">
     <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
       <h2 className="text-3xl font-bold text-center text-orange-500 mb-6">
         Create Account
